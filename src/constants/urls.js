4615d4203342ff44873eb77c62d976a5f6181246
Ayshath Ashfiya A. <ayshathshfiyaa@gmail.com>
2024-09-21 12:18:57 +0530

    urls changed

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,12 +1,12 @@
 
-export const API_BASE_URL = `http://127.0.0.1:8000/api/`;
+export const API_BASE_URL = `https://kidsschool.life/api/`;
 
-export const ATTENDANCE = `http://127.0.0.1:8000/attendance/`;
+export const ATTENDANCE = `https://kidsschool.life/attendance/`;
 
-export const RESULT = `http://127.0.0.1:8000/result/`;
+export const RESULT = `https://kidsschool.life/result/`;
 
-export const CHAT = `http://127.0.0.1:8000/chat/`;
+export const CHAT = `https://kidsschool.life/chat/`;
 
-export const LEAVE = `http://127.0.0.1:8000/leave/`; 
+export const LEAVE = `https://kidsschool.life/leave/`; 
 
-export const SOCKET = 'ws://127.0.0.1:8000/ws';
+export const SOCKET = 'ws://kidsschool.life/ws';
