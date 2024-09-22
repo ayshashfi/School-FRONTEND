84af4fd5a03d5b8b043f22da0cf38c6e2cca6c6e
Ayshath Ashfiya A. <ayshathshfiyaa@gmail.com>
2024-09-22 14:11:38 +0530

    url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiInstance = axios.create({
-    baseURL: 'https//kidsschool.life/api/',
+    baseURL: 'https://kidsschool.life/api/',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
