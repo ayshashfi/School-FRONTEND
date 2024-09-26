b9ba29cf7ec48c329b68df044179f02903091158
Ayshath Ashfiya A. <ayshathshfiyaa@gmail.com>
2024-09-26 10:38:02 +0530

    url

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,17 @@
 
+// export const API_BASE_URL = `http://127.0.0.1:8000/api/`;
+
+// export const ATTENDANCE = `http://127.0.0.1:8000/attendance/`;
+
+// export const RESULT = `http://127.0.0.1:8000/result/`;
+
+// export const CHAT = `http://127.0.0.1:8000/chat/`;
+
+// export const LEAVE = `http://127.0.0.1:8000/leave/`; 
+
+// export const SOCKET = 'ws://127.0.0.1:8000/ws';
+
+
 export const API_BASE_URL = `https://kidsschool.life/api/`;
 
 export const ATTENDANCE = `https://kidsschool.life/attendance/`;
@@ -9,4 +22,4 @@ export const CHAT = `https://kidsschool.life/chat/`;
 
 export const LEAVE = `https://kidsschool.life/leave/`; 
 
-export const SOCKET = 'ws://kidsschool.life/ws';
+export const SOCKET = 'wss://kidsschool.life/ws';
